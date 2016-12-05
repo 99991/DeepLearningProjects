a62cb108a43f352e1f31c15358a3125e377b7d43
Michael Janschek <michael.janschek@gmx.de>
2016-12-05 16:38:53 +0100

    prepare big test

--- projects/test/test_runner.py
+++ projects/test/test_runner.py
@@ -4,27 +4,27 @@ import copy
 import subprocess
 
 default_args = {
-	"batch_size":                 64,
-	"num_kernels1":                4,
-	"num_hidden":                100,
+	"batch_size":               1024,
+	"num_kernels1":               16,
+	"num_hidden":               1024,
 	"regularization_factor":    1e-4,
 	"dropout_keep_probability":  0.5,
 	"learning_rate":           0.001,
-	"kernel1_size":                3,
+	"kernel1_size":                5,
 	"test_interval":             100,
-	"num_batches":              6001,
+	"num_batches":             10001,
 	"seed":                      666,
-        "pool":                        4,
+    "pool":                        4,
 }
 
 test_values = {
     "batch_size":              [2048, 1024, 512, 256, 128, 64, 32, 16],
-    "num_kernels1":            [32, 16, 8, 4, 2, 1],
-    "num_hidden":              [2048, 1024, 512, 256, 128, 64, 32, 16],
-    "regularization_factor":   [10, 1, 0, 1, 1e-2, 1e-3, 1e-4, 1e-6, 1e-7],
+    "num_kernels1":            [64, 32, 16, 8, 4, 2, 1],
+    "num_hidden":              [4096, 2048, 1024, 512, 256, 128, 64, 32, 16],
+    #"regularization_factor":   [10, 1, 0, 1, 1e-2, 1e-3, 1e-4, 1e-6, 1e-7],
     "dropout_keep_probability":[0.1, 0.25, 0.5, 0.9],
     "learning_rate":           [1e-2, 1e-3, 1e-4, 1e-6],
-    "kernel1_size":            [7, 5, 3],
+    "kernel1_size":            [11, 9, 7, 5, 3],
     "pool":                    [1, 2, 4],
 }
 
